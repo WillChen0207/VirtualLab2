ec74df71702aeae9ffef34f5e21fa56696b18f56
WillChen0207 <2609846452@qq.com>
2022-04-19 18:00:04 +0800

    初始化更新

--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # Three.js Template / Boilerplate
 
 *Get started with Three.js to render 3d Web Experience*
@@ -49,3 +50,23 @@ Youtube Video: [Twitter](https://youtu.be/MmjZG-UX30g)
 [MIT](LICENSE)
 
 Made with :blue_heart: by [Mohnish Landge](http://mohnishlandge.me)
+=======
+<h1>运河杯物理虚拟实验室项目</h1><br>
+<h2>基础实现目标</h2>
+1、模拟物理实验环境<br>
+2、模拟物理实验器材<br>
+3、追踪实验者的操作数据<br>
+4、即时反馈与追踪评估<br>
+另附：请记得提交代码时附上更新项目日志
+
+----------------------------------------
+2022/3/11 updated
+<h2>一些项目的想法</h2>
+以电学实验为基础设计方向
+1、基本模型参考我的世界（Minecraft）和装机模拟器（PC Building Simulator），采取物品栏选择元件，同时在场景中可以添加元件的位置采用高亮表示<br>
+2、线缆连接采用预设的方式，即只允许使用者选择链接的端点，选择好后自动连接线条<br>
+3、需要使用sprite精灵模型等方式建立恰当的数据监测与反馈，从而给用户适当的反馈<br>
+4、可能需要自行建模与调整纹理<br>
+
+请先学习如何使用git（本人使用的是sourcetree），以及基础的threejs语法，如果有兴趣可以b站视频学习blender建模。
+>>>>>>> 9b10d99c729ef801bdb068e5ab6209a799622f5f
--- src/index.js
+++ src/index.js
@@ -1,31 +1,170 @@
 import './style/main.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader }  from 'three/examples/jsm/loaders/GLTFLoader.js'
+import * as CANNON from 'cannon'
 /**
  * GUI Controls
  */
 import * as dat from 'dat.gui'
 const gui = new dat.GUI()
+var sceneAttribute = new function() {
+  this.ballheight = 150;
+  this.ballmass = 0.5;
+}
+
+gui.add(sceneAttribute, 'ballmass', -2, 2).listen().onChange(function(value){
+  sphereBody.mass = value
+})
+gui.add(sceneAttribute, 'ballheight', 0, 200).listen().onChange(function(value){
+  ballheight = value
+})
 
 /**
  * Base
  */
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector('#webgl')
+var titlearray = ['_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','🚗'];
+var timer = window.setInterval(function(){
+  titlechange();
+}, 1000);
+function titlechange(){
+  var tmp = titlearray[0];
+  var titleText = "";
+  for (var i = 0; i <= 19; i++){
+    titlearray[i] = titlearray[i+1];
+  }
+  titlearray[20] = tmp;
+  for (var i = 0; i <= 20; i++){
+    titleText += titlearray[i];
+  }
+  document.getElementById("titletext").innerHTML = titleText;
+}
 
 // Scene
 const scene = new THREE.Scene()
+scene.background = new THREE.Color(0xdddddd)//场景背景色
+scene.fog = new THREE.Fog( 0xf2f7ff, 1, 100000 );
+
+//初始化cannonjs物理世界
+const world = new CANNON.World();
+const G = -9.82;
+world.gravity.set(0, 0.0001*G, 0);
+world.broadphase = new CANNON.NaiveBroadphase();
+world.solver.iterations = 10;
+world.solver.tolerance = 0.01;
+
+
+function Init(){
+  //创建刚体球体
+  sphereBody = new CANNON.Body({ 
+    mass: 0.5 ,
+    position: new CANNON.Vec3(0, ballheight, 0),
+    shape: sphereShape,
+    material: new CANNON.Material({
+      friction: 0.5,
+      restitution: 0.8
+    })
+  });
+  sphereBody.velocity.set(0, 0, 0);
+  sphereBody.angularVelocity.set(0, 0, 0);
+  sphereBody.force.set(0, 0, 0);
+  sphereBody.addShape(sphereShape);
+  world.add(sphereBody);
+  //创建上面球体的网格
+  sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  sphereMesh.position.set(0, ballheight, 0);
+  scene.add(sphereMesh);
+  update();
+}
+
+//创建刚体球体
+var ballheight = 150;
+var sphereShape = new CANNON.Sphere(8);
+var sphereBody = new CANNON.Body({ 
+  mass: 0.5 ,
+  position: new CANNON.Vec3(0, ballheight, 0),
+  shape: sphereShape,
+  material: new CANNON.Material({
+    friction: 0.5,
+    restitution: 0.8
+  })
+});
+sphereBody.addShape(sphereShape);
+world.add(sphereBody);
+
+//创建上面球体的网格
+var sphereGeometry = new THREE.SphereGeometry(8, 10, 10);
+var sphereMaterial = new THREE.MeshLambertMaterial({ color: 0x0000ff });
+var sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphereMesh.position.set(0, ballheight, 0);
+scene.add(sphereMesh);
+
+
+//创建地面刚体
+var planeShape = new CANNON.Plane();
+var planeBody = new CANNON.Body({
+  mass: 0,
+  shape: planeShape,
+  position: new CANNON.Vec3(0, 0, 0),
+  quaternion: new CANNON.Quaternion().setFromEuler( - Math.PI / 2, 0, 0),
+  material: new CANNON.Material({
+    restitution: 1,
+    friction: 0.5
+  })
+});
+planeBody.addShape(planeShape);
+world.add(planeBody);
+
+//创建地面网格
+var planeGeometry = new THREE.PlaneGeometry(1000, 1000, 100, 100);
+var planeMaterial = new THREE.MeshLambertMaterial({ 
+  texture: new THREE.TextureLoader().load('./static/floortexture.jpg')
+});
+var planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+planeMesh.receiveShadow = true;
+planeMesh.rotation.z = -Math.PI / 2;
+planeMesh.position.y = -0.5;
+scene.add(planeMesh);
+
+//将上面创建的刚体球体和网格关联起来并为物理世界开启持续更新
+function update(){
+  requestAnimationFrame(update)
+  world.step(1 / 60) 
+  if (sphereMesh) { 
+    if (sphereMesh.position.y < 0) { // 如果球体落地
+      sphereMesh.position.y = 0;
+      sphereBody.velocity.set(0, 0, 0);
+      sphereBody.angularVelocity.set(0, 0, 0);
+    }
+    sphereMesh.position.copy(sphereBody.position) 
+    sphereMesh.quaternion.copy(sphereBody.quaternion) 
+  } 
+  if (planeMesh) { 
+    planeMesh.position.copy(planeBody.position) 
+    planeMesh.quaternion.copy(planeBody.quaternion) 
+  }
+}
+
+
 
 /**
  * Object
  */
-const geometry = new THREE.IcosahedronGeometry(20, 1)
-const material = new THREE.MeshNormalMaterial()
-// Material Props.
-material.wireframe = true
-// Create Mesh & Add To Scene
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+
+//创建键盘触发事件
+function onKeyDown(event) {
+  switch (event.keyCode) {
+    case 38:{ // 删除sphereMesh并重新生成
+      scene.remove(sphereMesh);
+      world.remove(sphereBody);
+      Init();
+      break;
+    }
+  }
+}
+document.addEventListener('keydown', onKeyDown, false);
 
 /**
  * Sizes
@@ -57,17 +196,17 @@ const camera = new THREE.PerspectiveCamera(
   75,
   sizes.width / sizes.height,
   0.001,
-  5000
+  20000
 )
 camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 50
+camera.position.y = 20
+camera.position.z = 200
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
-controls.autoRotate = true
+controls.autoRotate = false
 // controls.enableZoom = false
 controls.enablePan = false
 controls.dampingFactor = 0.05
@@ -77,12 +216,29 @@ controls.touches = {
   ONE: THREE.TOUCH.ROTATE,
   TWO: THREE.TOUCH.DOLLY_PAN,
 }
+
+//添加一个环境光
+const ambientLight = new THREE.AmbientLight(0xffffff, 1)
+scene.add(ambientLight);
+//添加一个点光源
+const pointLight = new THREE.DirectionalLight(0xff0000, 1);
+pointLight.position.set(0, 50, 0);
+let helper = new THREE.DirectionalLightHelper(pointLight, 10);
+scene.add(pointLight);
+scene.add(helper);
+
+
+// 辅助坐标
+// var axesHelper = new THREE.AxesHelper( 150 );
+// scene.add( axesHelper );
+
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
   antialias: true,
+  backgroundColor: 0xffffff
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -94,14 +250,13 @@ const clock = new THREE.Clock()
 const tick = () => {
   const elapsedTime = clock.getElapsedTime()
 
-  //mesh.rotation.y += 0.01 * Math.sin(1)
-  //mesh.rotation.y += 0.01 * Math.sin(1)
-  mesh.rotation.z += 0.01 * Math.sin(1)
 
   // Update controls
   controls.update()
+
   // Render
   renderer.render(scene, camera)
+  update()
 
   // Call tick again on the next frame
   window.requestAnimationFrame(tick)
